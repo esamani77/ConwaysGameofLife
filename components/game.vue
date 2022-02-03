7384c5ddf1bd272b2081e72ac38ecba10e373964
esamani77 <esamani77@gmail.com>
2022-02-04 01:24:11 +0330

    fix $ref canvas

--- components/game.vue
+++ components/game.vue
@@ -4,56 +4,52 @@
 import { ref, onMounted } from "vue";
 
 const gamefield = ref(null);
-// const canvas = this.ref.gamefield;
-// const ctx = canvas.getContext("2d");
+let canvas = null;
+let ctx = null;
 let cell_size = 5;
-let dead_color = `#181818`;
-let alive_color = `#FF756B`;
+let dead_color = `#000`;
+let alive_color = `#FFF`;
 let cells_in_column = Math.floor(1400 / cell_size);
 let cells_in_rows = Math.floor(500 / cell_size);
 let active_array = new Array();
 let inactive_array = [];
 function color(status) {}
 let arrayInitialization = () => {
-  for (let i = 0; i < this.cells_in_rows; i++) {
-    this.active_array[i] = [];
-    for (let j = 0; j < this.cells_in_column; j++) {
-      this.active_array[i][j] = 0;
+  for (let i = 0; i < cells_in_rows; i++) {
+    for (let j = 0; j < cells_in_column; j++) {
+      active_array[i][j] = 0;
     }
   }
-  this.inactive_array = this.active_array;
+  inactive_array = active_array;
 };
 
 let arrayRandomize = () => {
   for (let i = 0; i < cells_in_rows; i++) {
+    active_array[i] = [];
     for (let j = 0; j < cells_in_column; j++) {
-      console.log(active_array);
       active_array[i][j] = 0;
       active_array[i][j] = Math.random() > 0.5 ? 1 : 0;
     }
   }
+
+  inactive_array = active_array;
 };
 
 let fillArray = () => {
-  for (let i = 0; i < this.cells_in_rows; i++) {
-    for (let j = 0; j < this.cells_in_column; j++) {
+  for (let i = 0; i < cells_in_rows; i++) {
+    for (let j = 0; j < cells_in_column; j++) {
       let color;
-      if (this.active_array[i][j] == 1) color = this.alive_color;
-      else color = this.dead_color;
+      if (active_array[i][j] == 1) color = alive_color;
+      else color = dead_color;
       ctx.fillStyle = color;
-      ctx.fillRect(
-        j * this.cell_size,
-        i * this.cell_size,
-        this.cell_size,
-        this.cell_size
-      );
+      ctx.fillRect(j * cell_size, i * cell_size, cell_size, cell_size);
     }
   }
 };
 
 let setCellValueHelper = (row, col) => {
   try {
-    return this.active_array[row][col];
+    return Boolean(active_array[row][col]) ? active_array[row][col] : 0;
   } catch {
     return 0;
   }
@@ -61,67 +57,64 @@ let setCellValueHelper = (row, col) => {
 
 let countNeighbours = (row, col) => {
   let total_neighbours = 0;
-  total_neighbours += this.setCellValueHelper(row - 1, col - 1);
-  total_neighbours += this.setCellValueHelper(row - 1, col);
-  total_neighbours += this.setCellValueHelper(row - 1, col + 1);
-  total_neighbours += this.setCellValueHelper(row, col - 1);
-  total_neighbours += this.setCellValueHelper(row, col + 1);
-  total_neighbours += this.setCellValueHelper(row + 1, col - 1);
-  total_neighbours += this.setCellValueHelper(row + 1, col);
-  total_neighbours += this.setCellValueHelper(row + 1, col + 1);
+  total_neighbours += setCellValueHelper(row - 1, col - 1);
+  total_neighbours += setCellValueHelper(row - 1, col);
+  total_neighbours += setCellValueHelper(row - 1, col + 1);
+  total_neighbours += setCellValueHelper(row, col - 1);
+  total_neighbours += setCellValueHelper(row, col + 1);
+  total_neighbours += setCellValueHelper(row + 1, col - 1);
+  total_neighbours += setCellValueHelper(row + 1, col);
+  total_neighbours += setCellValueHelper(row + 1, col + 1);
   return total_neighbours;
 };
 
 let updateCellValue = (row, col) => {
-  const total = this.countNeighbours(row, col);
+  const total = countNeighbours(row, col);
   // cell with more than 4 or less then 3 neighbours dies. 1 => 0; 0 => 0
   if (total > 4 || total < 3) {
     return 0;
   }
   // dead cell with 3 neighbours becomes alive. 0 => 1
-  else if (this.active_array[row][col] === 0 && total === 3) {
+  else if (active_array[row][col] === 0 && total === 3) {
     return 1;
   }
   // or returning its status back. 0 => 0; 1 => 1
   else {
-    return this.active_array[row][col];
+    return active_array[row][col];
   }
 };
 
 let updateLifeCycle = () => {
-  for (let i = 0; i < this.cells_in_rows; i++) {
-    for (let j = 0; j < this.cells_in_column; j++) {
-      let new_state = this.updateCellValue(i, j);
-      this.inactive_array[i][j] = new_state;
+  for (let i = 0; i < cells_in_rows; i++) {
+    for (let j = 0; j < cells_in_column; j++) {
+      let new_state = updateCellValue(i, j);
+      inactive_array[i][j] = new_state;
     }
   }
-  this.active_array = this.inactive_array;
+  active_array = inactive_array;
 };
 
-let gameSetUp = () => {
-  this.arrayInitialization();
-};
+// let gameSetUp = () => {
+//   arrayInitialization();
+// };
 
 let runGame = () => {
-  this.updateLifeCycle()``;
-  this.fillArray();
+  updateLifeCycle();
+  fillArray();
 };
-onMounted(() => {
-  // the DOM element will be assigned to the ref after initial render
-  console.log("gamefield.value"); // <div>This is a gamefield element</div>
-  console.log(gamefield.value); // <div>This is a gamefield element</div>
-});
 let asss = () => {
+  canvas = gamefield.value;
+  ctx = canvas.getContext("2d");
   arrayRandomize();
-  // this.fillArray();
-  // setInterval(() => {
-  //   this.runGame();
-  // }, 1);
+  fillArray();
+  setInterval(() => {
+    runGame();
+  }, 1);
 };
 </script>
 <template>
   <div>
-    <button @click="asss">ssss</button>
+    <button @click="asss">run game</button>
     <canvas ref="gamefield" id="gamefield" width="1400" height="500"></canvas>
   </div>
 </template>
